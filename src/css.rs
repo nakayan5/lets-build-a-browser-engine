e5c084a5be3c3c5198083cf2ea5a955bba770a11
nakayan5 <tomoya.progress@gmail.com>
2023-12-17 20:51:07 +0900

    update: css

--- src/css.rs
+++ src/css.rs
@@ -196,7 +196,7 @@ impl Parser {
 
         Declaration {
             name: property_name,
-            value: value,
+            value,
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+pub mod css;
 pub mod dom;
+pub mod html;
 
 fn main() {
-    println!("Hello, world!");
+    // let var = dom::text("Hello, world!".to_string());
+    // println!("----------- {:?}", var);
+
+    let node =
+        html::parse("<html><body><h1>hello</h1><div><p>world</p></div></body></html>".to_string());
+
+    println!("----------- {:?}", node);
 }
